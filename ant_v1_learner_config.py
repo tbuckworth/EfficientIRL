f82a3a3ee03fd05538f389f338dfcab881c24414
tbuckworth <elkilesh@hotmail.co.uk>
2025-03-03 12:03:48 -0500

    reverted make_vec_env

--- ant_v1_learner_config.py
+++ ant_v1_learner_config.py
@@ -60,22 +60,35 @@ def load_ant_learner(env, logdir=None):
         device="auto",
         _init_setup_model=True,
     )
-    # SAC
     learner = PPO(**ant_params)
     return learner
 
 
 def load_ant_sac_learner(env, logdir, policy):
+    OrderedDict([('batch_size', 512),
+                 ('buffer_size', 1000000),
+                 ('gamma', 0.98),
+                 ('learning_rate', 0.0018514039303149058),
+                 ('learning_starts', 1000),
+                 ('n_timesteps', 1000000.0),
+                 ('policy', 'MlpPolicy'),
+                 ('policy_kwargs',
+                  {'log_std_init': -2.2692589009754176,
+                   'net_arch': [256, 256],
+                   'use_sde': False}),
+                 ('tau', 0.05),
+                 ('train_freq', 64),
+                 ('normalize', False)])
     params = dict(
-        policy="SACPolicy",
+        policy='MlpPolicy',
         env=env,
-        learning_rate=3e-4,
+        learning_rate=0.0018514039303149058,
         buffer_size=1_000_000,  # 1e6
-        learning_starts=100,
-        batch_size=256,
-        tau=0.005,
-        gamma=0.99,
-        train_freq=1,
+        learning_starts=1000,
+        batch_size=512,
+        tau=0.05,
+        gamma=0.98,
+        train_freq=64,
         gradient_steps=1,
         action_noise=None,
         replay_buffer_class=None,
@@ -88,8 +101,10 @@ def load_ant_sac_learner(env, logdir, policy):
         sde_sample_freq=-1,
         use_sde_at_warmup=False,
         stats_window_size=100,
-        tensorboard_log=None,
-        policy_kwargs=None,
+        tensorboard_log=logdir,
+        policy_kwargs={'log_std_init': -2.2692589009754176,
+                       'net_arch': [32, 32],
+                       'use_sde': False},
         verbose=0,
         seed=None,
         device="auto",
@@ -97,4 +112,3 @@ def load_ant_sac_learner(env, logdir, policy):
     )
     learner = SAC(**params)
     learner.policy.actor = policy.actor
-
