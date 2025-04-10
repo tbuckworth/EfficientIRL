0d9cffcab882f4ed6d79cf87ad624180ecb06e6d
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-10 15:29:49 +0100

    trying hybrid meow

--- meow_learner.py
+++ meow_learner.py
@@ -1,7 +1,7 @@
 import numpy as np
 from stable_baselines3.common.evaluation import evaluate_policy
 
-from helper_local import create_envs_meow_imitation_compat
+from helper_local import create_envs_meow_imitation_compat, create_logdir
 from meow.meow_continuous_action import HybridPolicy, MEOW, create_envs_meow
 from private_login import wandb_login
 import wandb
@@ -22,12 +22,13 @@ def train_class():
     cfg = locals()
     wandb_login()
     project = "EfficientIRL"
+    logdir = create_logdir(env_name, seed)
     wandb.init(project=project, config=cfg, sync_tensorboard=True,
                tags=cfg["tags"], resume="allow")
 
     envs, test_envs = create_envs_meow_imitation_compat(env_name, n_envs, norm_reward, seed)
     # envs, test_envs = create_envs_meow(env_name, seed, n_envs)
-    learner = MEOW(envs, test_envs, policy_constructor=HybridPolicy, evaluate=evaluate)
+    learner = MEOW(envs, test_envs, policy_constructor=HybridPolicy, evaluate=evaluate, logdir=logdir)
     learner.learn(100_000, wandb=wandb)
 
 if __name__ == "__main__":
