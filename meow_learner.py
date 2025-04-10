114548a27f59b4eabeb9712c02d6086118429114
tbuckworth <elkilesh@hotmail.co.uk>
2025-04-10 14:54:35 +0100

    trying hybrid meow

--- meow_learner.py
+++ meow_learner.py
@@ -1,17 +1,19 @@
 from helper_local import create_envs_meow_imitation_compat
-from meow.meow_continuous_action import HybridPolicy, MEOW
+from meow.meow_continuous_action import HybridPolicy, MEOW, create_envs_meow
 from private_login import wandb_login
 
 
 def train_class():
     import wandb
-    env_name = "seals:seals/Hopper-v1"
+    # env_name = "seals:seals/Hopper-v1"
+    env_name = "Hopper-v5"
     n_envs = 8
     norm_reward = False
     seed = 42
     wandb_login()
 
-    envs, test_envs = create_envs_meow_imitation_compat(env_name, n_envs, norm_reward, seed)
+    # envs, test_envs = create_envs_meow_imitation_compat(env_name, n_envs, norm_reward, seed)
+    envs, test_envs = create_envs_meow(env_name, seed, n_envs)
     learner = MEOW(envs, test_envs, policy_constructor=HybridPolicy)
     learner.learn(100_000, wandb=wandb)
 
